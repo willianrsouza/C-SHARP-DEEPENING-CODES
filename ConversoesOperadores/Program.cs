24361dbe649172d388a49041a156c9d67e39532b
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-12 13:09:09 -0300

    conversões-operadores

---
+++ ConversoesOperadores/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConversoesOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Conversões
+
+            /* Base */
+            int notaAluno = 10;
+
+            /* Conversão Implicita */
+            double notaAlunoDouble = notaAluno;
+
+            /* Conversão Explicita*/
+            int numeroDoubleParaInt = (int)notaAlunoDouble;
+
+            /* Utilizando Convert*/
+            string notaString = "10";
+            int notaConvert = Convert.ToInt32(notaString);
+
+            /* Utilizando Convert*/
+            int notaParse = int.Parse(notaString);
+
+            if (int.TryParse(notaString, out int notaTryParse))
+            {
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
