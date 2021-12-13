fef53646559d6128cfb35e98fd074ca96bc5e886
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-12 22:40:50 -0300

    estrutura-repetição

---
+++ EstruturasRepeticao/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EstruturasRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region  Estrutuas de Repetição - For
+
+            Console.WriteLine("Digite Sequencia de Números");
+
+            var numerosTexto = Console.ReadLine();
+            var numeros = numerosTexto.Split(' ');
+
+            Console.WriteLine("Numeros: ");
+
+            /* Inicialização - Condição* - Incremento*/
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            #endregion
+
+            #region  Estrutuas de Repetição - While
+
+            var contador = 0;
+
+            while (contador < numeros.Length)
+            {
+                Console.WriteLine(numeros[contador]);
+            }
+
+            #endregion
+
+            #region  Estrutuas de Repetição - Foreach
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
