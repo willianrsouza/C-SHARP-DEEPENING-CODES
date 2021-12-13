3a9e5be26662b9360bb13b94f8e217083fbd7a5a
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-12 22:46:57 -0300

    operadores-comparacao

---
+++ OperadoresComparacao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperadoresComparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Operadores de Comparação 
+
+            /* > , >= , <, <= */
+
+            Console.WriteLine(4 > 5);
+            Console.WriteLine(5 > 5);
+            Console.WriteLine(5 >= 5);
+
+            Console.WriteLine(5 < 4);
+            Console.WriteLine(5 <= 5);
+            Console.WriteLine(5 < 6);
+
+            #endregion
+        }
+    }
+}
