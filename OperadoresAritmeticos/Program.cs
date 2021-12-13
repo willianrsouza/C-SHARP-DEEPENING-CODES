157d674cc7662fbecb1d76c1160c4b4a3e1380ee
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-12 22:50:11 -0300

    operadores-aritméticos

---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores Aritmeticos 
+
+            int numeroOperador = 4;
+
+
+            /* Operador de Incremento - Sufixado */
+            Console.WriteLine(numeroOperador++);
+            Console.WriteLine(numeroOperador--);
+
+            /* Operador de Incremento - Prefixado */
+            Console.WriteLine(++numeroOperador);
+            Console.WriteLine(--numeroOperador);
+
+            /* Operações de Logica*/
+            Console.WriteLine(numeroOperador);
+            Console.WriteLine(-numeroOperador);
+            Console.WriteLine(-(-numeroOperador));
+
+            /* Operadoes Binarios */
+
+            var soma = 4 + 5;
+            var subtração = 4 - 5;
+            var multiplicação = 4 * 5;
+            var divisao = 20 / 3;
+            var divisaoDouble = (double)20 / 5;
+            var multiplos = (4 + 5) + 10;
+
+            #endregion
+        }
+    }
+}
