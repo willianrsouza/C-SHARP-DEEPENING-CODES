162c7449a073eedd5c050857da75aac2dfbf8b1d
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-12 22:49:15 -0300

    operadores-logicos

---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores Logicos AND & OR 
+
+            /* AND */
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || true);
+            Console.WriteLine(false || false);
+
+            /* OR */
+            Console.WriteLine(true && false);
+            Console.WriteLine(false && true);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
