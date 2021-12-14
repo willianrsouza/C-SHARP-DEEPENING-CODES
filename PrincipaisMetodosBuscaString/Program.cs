90ad039012dd39eef8986c4ab128bd9011f26274
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-13 21:08:01 -0300

    principais-metodos-busca-string

---
+++ PrincipaisMetodosBuscaString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrincipaisMetodosBuscaString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frase = " C# é uma linguagem moderna e versátil." +
+                "Com C# consigo desenvolver para Web, Desktop, Jogos," +
+                "Mobile, entre outros. ";
+
+            /*IndexOf*/
+            var IndexOf = frase.IndexOf("C#");
+
+            /*LastIndexOf*/
+            var LastIndexOf = frase.LastIndexOf("C#");
+
+            /*StartWith*/
+            var startWith = frase.StartsWith("C#");
+
+            /*Substring*/
+            var IndexOfMobile = frase.IndexOf("Mobile");
+            var substring = frase.Substring(IndexOfMobile, 6);
+
+            /*Contains*/
+            var containsOrdinal = frase.Contains("C#", StringComparison.OrdinalIgnoreCase);
+            var contains = frase.Contains("C#");
+
+            Console.ReadKey();
+        }
+    }
+}
