55cd2496779a6c75d291e5dd05103c8b2aebe88b
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-15 19:50:27 -0300

    operações-date-time

---
+++ DateTime/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TrabalhandoComDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DateTime
+            /* Buscar Data atual */
+            var now = DateTime.Now;
+
+            var today = DateTime.Today;
+
+            /* Buscar Data três dias atrás */
+            var treeDaysAgo = now.AddDays(-3);
+
+            /* Data de experiação, por meses */
+            var sixMonthsLater = today.AddMonths(6);
+
+            /* Data de experiação de dois anos*/
+            var twoYearsLater = today.AddYears(2);
+
+            /* Opções de visualização/formatação utilizando data*/
+            var shortDate = now.ToShortDateString();
+            var longDate = now.ToLongDateString();
+
+            var shortTime = now.ToShortTimeString();
+            var longTime = now.ToLongTimeString();
+
+            var date = now.Date;
+            var day = now.Day;
+            var month = now.Day;
+            var year = now.Day;
+            var hour = now.Day;
+            var minute = now.Day;
+            var second = now.Day;
+
+            /* Comparando as datas*/
+            var dayOfWeek = now.DayOfWeek;
+
+            if (dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("É fim de Semana.");
+            }
+
+            /* Data do Ano*/
+            var dayofYear = now.DayOfYear;
+
+            #endregion DateTime
+
+            Console.ReadKey();
+        }
+    }
+}
