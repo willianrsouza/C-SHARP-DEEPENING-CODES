c84dbb84d62fd493e0271cf548fd16d9fb880907
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-16 11:04:53 -0300

    formatacao-date-time

---
+++ FormatacaoDateTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FormatacaoDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region DateTime Formatação
+
+            DateTime now = DateTime.Now;
+
+            var formats = new string[] {"d", "D", "f", "F", "G",
+                "m", "o", "r", "T", "u", "V"};
+
+            foreach (var format in formats)
+            {
+                Console.WriteLine("Data no formato {0}: {1}", format, now.ToString(format));
+                //Console.WriteLine($"Data no formato {format}: {now.ToString(format)} ");
+            }
+
+            Console.WriteLine($"Data no Formato d: {now:d} ");
+            Console.WriteLine($"Data no Formato MM-dd-yyyy: {now:MM-dd-yyyy} ");
+
+            var dateFormat = now.ToString("MM-dd-yyyy");
+            var dateFormatBr = now.ToString("dd/MM/yyyy HH:mm:ss");
+
+            Console.ReadKey();
+
+            #endregion
+
+
+        }
+    }
+}
