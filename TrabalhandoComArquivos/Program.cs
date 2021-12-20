78eae8d330cec636d337c2e13b23b897445bb02e
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2021-12-20 16:32:07 -0300

    manipulando-arquivos

---
+++ TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region File
+
+            var file = @"pasta\texto.txt";
+
+            if (!File.Exists(file))
+            {
+                File.Create(file);
+            }
+
+            var fileInfo = new FileInfo(file);
+
+            Console.WriteLine($"Nome: {fileInfo.Name}, Tamanho: {fileInfo.Length}, Data de Atualização: {fileInfo.LastWriteTime}");
+
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
